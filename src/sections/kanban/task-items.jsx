791e4c828d254389b8f09b339ea5b9a4000ca261
Aravind T <aravind@payfinix.in>
2024-02-21 11:20:54 +0530

    kanban

---
+++ src/sections/kanban/task-items.jsx
@@ -0,0 +1,117 @@
+import { Avatar, AvatarGroup, Box, Paper, Stack, Typography } from '@mui/material';
+import Iconify from 'src/components/iconify/Iconify';
+import Image from 'src/components/image';
+
+export default function TaskItem({ item, provided }) {
+    const renderPriority = (
+        <Iconify
+            icon={
+                (item.priority === 'low' && 'solar:double-alt-arrow-down-bold-duotone') ||
+                (item.priority === 'medium' && 'solar:double-alt-arrow-right-bold-duotone') ||
+                'solar:double-alt-arrow-up-bold-duotone'
+            }
+            sx={{
+                position: 'absolute',
+                top: 4,
+                right: 4,
+                ...(item.priority === 'low' && {
+                    color: 'info.main',
+                }),
+                ...(item.priority === 'medium' && {
+                    color: 'warning.main',
+                }),
+                ...(item.priority === 'high' && {
+                    color: 'error.main',
+                }),
+            }}
+        />
+    );
+    const renderImg = (
+        <Box
+            sx={{
+                p: (theme) => theme.spacing(1, 1, 0, 1),
+            }}
+        >
+            <Image
+                disabledEffect
+                alt={item.attachments[0]}
+                src={item.attachments[0]}
+                ratio="4/3"
+                sx={{
+                    borderRadius: '14px',
+                }}
+            />
+        </Box>
+    );
+
+    const renderInfo = (
+        <Stack direction="row" alignItems="center">
+            <Stack
+                flexGrow={1}
+                direction="row"
+                alignItems="center"
+                sx={{
+                    typography: 'caption',
+                    color: 'text.disabled',
+                }}
+            >
+                <Iconify width={16} icon="solar:chat-round-dots-bold" sx={{ mr: 0.25 }} />
+                <Box component="span" sx={{ mr: 1 }}>
+                    {item.comments.length}
+                </Box>
+
+                <Iconify width={16} icon="eva:attach-2-fill" sx={{ mr: 0.25 }} />
+                <Box component="span">{item.attachments.length}</Box>
+            </Stack>
+            {item.users && (
+                <AvatarGroup total={4}>
+                    {item.users.map((user) => (
+                        <Avatar
+                            alt="Remy Sharp"
+                            src={user.avatar}
+                            sx={{
+                                width: 16,
+                                height: 16,
+                            }}
+                        />
+                    ))}
+                </AvatarGroup>
+            )}
+        </Stack>
+    );
+    return (
+        <Paper
+            ref={provided.innerRef}
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+            // onClick={details.onTrue}
+            sx={{
+                width: 1,
+                borderRadius: '16px',
+                overflow: 'hidden',
+                position: 'relative',
+                boxShadow:
+                    'rgba(9, 30, 66, 0.25) 0px 1px 1px, rgba(9, 30, 66, 0.13) 0px 0px 1px 1px',
+                '&:hover': {
+                    boxShadow: 'rgba(149, 157, 165, 0.2) 0px 8px 24px',
+                },
+            }}
+        >
+            {!!item.attachments.length && renderImg}
+            <Stack spacing={2} sx={{ px: 2, py: 1.5, position: 'relative' }}>
+                {renderPriority}
+
+                <Typography
+                    variant="subtitle2"
+                    sx={{
+                        textTransform: 'capitalize',
+                        color: '#212B36',
+                    }}
+                >
+                    {item.content}
+                </Typography>
+                {renderInfo}
+            </Stack>
+        </Paper>
+    );
+}
--- src/sections/kanban/task-overview.jsx
+++ src/sections/kanban/task-overview.jsx
@@ -2,12 +2,15 @@ import { useState, useEffect } from 'react';
 import { DragDropContext, Droppable, Draggable } from '@hello-pangea/dnd';
 import { Avatar, AvatarGroup, Box, Card, Stack, Typography } from '@mui/material';
 import Iconify from 'src/components/iconify/Iconify';
+import TaskItem from './task-items';
 
 const getItems = (count, prefix) =>
     Array.from({ length: count }, (v, k) => k).map((k) => ({
         id: `${prefix}-item-${k}`,
         content: `item ${k}`,
         priority: 'high',
+        comments: [],
+        attachments: [],
         users: [
             {
                 userid: 1,
@@ -39,6 +42,7 @@ export default function TaskKanbanView() {
         { id: 'todo', name: 'To Do', value: [...getItems(5, 'todo')] },
         { id: 'inProgress', name: 'In Progress', value: [...getItems(5, 'inProgress')] },
         { id: 'done', name: 'Done', value: [...getItems(5, 'done')] },
+        { id: 'new', name: 'New', value: [...getItems(5, 'new')] },
     ]);
 
     const reorder = (list, startIndex, endIndex) => {
@@ -81,8 +85,8 @@ export default function TaskKanbanView() {
     };
 
     return (
-        <DragDropContext onDragEnd={onDragEnd}>
-            <Stack gap={3} direction="row" sx={{ m: 3 }}>
+        <Stack gap={3} direction="row" sx={{width: 1150, overflowX: 'scroll' }}>
+            <DragDropContext onDragEnd={onDragEnd}>
                 {columns.map((column) => (
                     <Droppable key={column.id} droppableId={column.id}>
                         {(provided, snapshot) => (
@@ -92,13 +96,21 @@ export default function TaskKanbanView() {
                                 sx={{
                                     background: '#f4f6f8',
                                     p: 2,
-                                    width: 250,
-                                    borderRadius: '8px',
+                                    borderRadius: '16px',
                                 }}
-                                gap={3}
                             >
-                                <Typography>{column.name}</Typography>
-                                <Stack gap={2}>
+                                <Typography
+                                    variant="subtitle1"
+                                    sx={{
+                                        textTransform: 'capitalize',
+                                        color: '#212B36',
+                                        fontWeight: 'bold',
+                                    }}
+                                    gutterBottom
+                                >
+                                    {column.name}
+                                </Typography>
+                                <Stack spacing={2} sx={{ width: 280, py: 3 }}>
                                     {column.value.map((item, index) => (
                                         <Draggable
                                             key={item.id}
@@ -106,55 +118,7 @@ export default function TaskKanbanView() {
                                             index={index}
                                         >
                                             {(provided, snapshot) => (
-                                                <Stack
-                                                    ref={provided.innerRef}
-                                                    component={Card}
-                                                    {...provided.draggableProps}
-                                                    {...provided.dragHandleProps}
-                                                    style={{
-                                                        userSelect: 'none',
-                                                        padding: grid * 2,
-                                                        background: snapshot.isDragging
-                                                            ? 'transparent'
-                                                            : 'white',
-                                                        boxShadow: snapshot.isDragging
-                                                            ? 'rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px;'
-                                                            : 'rgba(27, 31, 35, 0.04) 0px 1px 0px, rgba(255, 255, 255, 0.25) 0px 1px 0px inset',
-                                                        backdropFilter: snapshot.isDragging
-                                                            ? 'blur(1px)'
-                                                            : 'none',
-                                                        position: 'relative',
-                                                        ...provided.draggableProps.style,
-                                                    }}
-                                                >
-                                                    <Iconify
-                                                        icon="f7:status"
-                                                        sx={{
-                                                            color:
-                                                                (item.priority === 'high' &&
-                                                                    'darkred') ||
-                                                                (item.priority === 'medium' &&
-                                                                    'yellow') ||
-                                                                (item.priority === 'low' &&
-                                                                    'green'),
-                                                            position: 'absolute',
-                                                            top: 8,
-                                                            right: 8,
-                                                        }}
-                                                    />
-                                                    <Typography>{item.content}</Typography>
-                                                    {item.users && (
-                                                        <AvatarGroup total={4}>
-                                                            {item.users.map((user) => (
-                                                                <Avatar
-                                                                    alt="Remy Sharp"
-                                                                    src={user.avatar}
-                                                                    sx={{ width: 16, height: 16 }}
-                                                                />
-                                                            ))}
-                                                        </AvatarGroup>
-                                                    )}
-                                                </Stack>
+                                                <TaskItem item={item} provided={provided} />
                                             )}
                                         </Draggable>
                                     ))}
@@ -164,7 +128,7 @@ export default function TaskKanbanView() {
                         )}
                     </Droppable>
                 ))}
-            </Stack>
-        </DragDropContext>
+            </DragDropContext>
+        </Stack>
     );
 }
